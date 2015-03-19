6f4a52d2c0ebb769cf66046da2792d248bd5452e
cbj <issic47@gmail.com>
2015-03-19 20:45:45 +0800

    fix a bug in timer queue

--- muduo/net/TimerQueue.cc
+++ muduo/net/TimerQueue.cc
@@ -101,7 +101,7 @@ void TimerQueue::cancelInLoop(TimerId timerId)
   if (timer) 
   {
     timer->stop();
-    allocTimers_.remove(timer);
+    loop_->queueInLoop(boost::bind(&TimerList::remove, allocTimers_, timer));
   }
   else
   {
